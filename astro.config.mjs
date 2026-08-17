f3b716bd4823668c9c3884c7a7b4b60e6c992cb4
Tiago Fernandes <tiagojfer@gmail.com>
2026-08-16 23:05:39 -0300

    perf: optimize PageSpeed metrics, font loading, webp images, robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://nfenglish.com.br',
+
   i18n: {
     defaultLocale: 'pt-BR',
     locales: ['pt-BR', 'en', 'ru'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+
+  integrations: [sitemap()]
 });
