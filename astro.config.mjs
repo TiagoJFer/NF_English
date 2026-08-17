14562b237d070e0b055a6deaa8cb71418b6c10f4
Tiago Fernandes <tiagojfer@gmail.com>
2026-08-16 22:45:05 -0300

    feat: add English and Russian versions with PT|EN|RU switcher and fix navbar layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://nfenglish.com.br',
+  i18n: {
+    defaultLocale: 'pt-BR',
+    locales: ['pt-BR', 'en', 'ru'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
